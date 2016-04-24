10cd56751de26e0244436ee778ffe15771117e57
aa6 <justsunshineandbluesky@gmail.com>
2016-04-24 21:01:21 +0300

    Reset stamina on player's death

--- init.lua
+++ init.lua
@@ -7,12 +7,12 @@ minetest_wadsprint =
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/config.lua")
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/init_hudbars.lua")
 
-function minetest_wadsprint.update_player_properties(player)
+function minetest_wadsprint.stamina_update_cycle(player)
     if player.is_sprinting then
         player.stamina = player.stamina - (minetest_wadsprint.STAMINA_MAX_VALUE * minetest_wadsprint.SPRINT_STAMINA_DECREASE_PER_UPDATE_PERIOD_COEFFICIENT)
         if player.stamina < 0 then
             player.stamina = 0
-            minetest_wadsprint.disable_sprinting(player)
+            minetest_wadsprint.set_sprinting(player,false)
         end
     elseif player.stamina < minetest_wadsprint.STAMINA_MAX_VALUE then
         player.stamina = player.stamina + (minetest_wadsprint.STAMINA_MAX_VALUE * minetest_wadsprint.SPRINT_STAMINA_INCREASE_PER_UPDATE_PERIOD_COEFFICIENT)
@@ -23,27 +23,22 @@ function minetest_wadsprint.update_player_properties(player)
     minetest_wadsprint.hudbar_update_stamina(player)
 end
 
-function minetest_wadsprint.enable_sprinting(player)
-    if not player.is_sprinting then
-        player.is_sprinting = true
-        player.obj:set_physics_override(
-        {
-            jump = minetest_wadsprint.SPRINT_JUMP_HEIGHT_MODIFIER_COEFFICIENT,
-            speed = minetest_wadsprint.SPRINT_SPEED_MODIFIER_COEFFICIENT,
-        })
-        minetest_wadsprint.hudbar_update_ready_to_sprint(player)
-        minetest_wadsprint.hudbar_update_stamina(player)
-    end
-end
-
-function minetest_wadsprint.disable_sprinting(player)
-    if player.is_sprinting then
-        player.is_sprinting = false
-        player.obj:set_physics_override(
-        {
-            jump = 1.0,
-            speed = 1.0,
-        })
+function minetest_wadsprint.set_sprinting(player,is_sprinting)
+    if player.is_sprinting ~= is_sprinting then
+        player.is_sprinting = is_sprinting
+        if is_sprinting then
+            player.obj:set_physics_override(
+            {
+                jump = minetest_wadsprint.SPRINT_JUMP_HEIGHT_MODIFIER_COEFFICIENT,
+                speed = minetest_wadsprint.SPRINT_SPEED_MODIFIER_COEFFICIENT,
+            })
+        else
+            player.obj:set_physics_override(
+            {
+                jump = 1.0,
+                speed = 1.0,
+            })
+        end
         minetest_wadsprint.hudbar_update_ready_to_sprint(player)
         minetest_wadsprint.hudbar_update_stamina(player)
     end
@@ -53,35 +48,43 @@ function minetest_wadsprint.set_ready_to_sprint(player,is_ready_to_sprint)
     if player.is_ready_to_sprint ~= is_ready_to_sprint then
         player.is_ready_to_sprint = is_ready_to_sprint
         minetest_wadsprint.hudbar_update_ready_to_sprint(player)
-    else
-        player.is_ready_to_sprint = is_ready_to_sprint
     end
 end
 
 function minetest_wadsprint.scan_player_controls(player)
     local control = player.obj:get_player_control()
     if not control["up"] then
-        minetest_wadsprint.disable_sprinting(player)
+        minetest_wadsprint.set_sprinting(player,false)
     end
     if control["left"] and control["right"] then
         minetest_wadsprint.set_ready_to_sprint(player,true)
         if control["up"] then
-            minetest_wadsprint.enable_sprinting(player)
+            minetest_wadsprint.set_sprinting(player,true)
         end
     else
         minetest_wadsprint.set_ready_to_sprint(player,false)
     end
 end
 
+function minetest_wadsprint.reset_stamina(player)
+    player.stamina = minetest_wadsprint.STAMINA_MAX_VALUE
+    minetest_wadsprint.set_sprinting(player,false)
+    minetest_wadsprint.set_ready_to_sprint(player,false)
+    return player
+end
+
 minetest.register_on_joinplayer(function(player_obj)
-    minetest_wadsprint.players[player_obj:get_player_name()] = 
+    local player = 
     { 
         obj = player_obj,
-        stamina = minetest_wadsprint.STAMINA_MAX_VALUE,
-        is_sprinting = false,
-        is_ready_to_sprint = false,
     }
-    minetest_wadsprint.initialize_hudbar(minetest_wadsprint.players[player_obj:get_player_name()])
+    minetest_wadsprint.players[player_obj:get_player_name()] = player   
+    minetest_wadsprint.initialize_hudbar(player)
+    minetest_wadsprint.reset_stamina(player)
+end)
+
+minetest.register_on_respawnplayer(function(player_obj)
+  minetest_wadsprint.reset_stamina(minetest_wadsprint.players[player_obj:get_player_name()])
 end)
 
 minetest.register_on_leaveplayer(function(player_obj)
@@ -107,7 +110,7 @@ minetest.register_globalstep(function(dtime)
     if timer_properties_update > minetest_wadsprint.PLAYER_STATS_UPDATE_PERIOD_SECONDS then
         timer_properties_update = 0
         for player_name,player in pairs(minetest_wadsprint.players) do
-            minetest_wadsprint.update_player_properties(player)
+            minetest_wadsprint.stamina_update_cycle(player)
         end
     end
 end)
