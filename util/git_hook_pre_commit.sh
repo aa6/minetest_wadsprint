2dcd927d0330a033cf60f7019645c48833235cdf
aa6 <justsunshineandbluesky@gmail.com>
2016-04-30 10:36:59 +0300

    Fixing comment typos

--- util/git_hook_pre_commit.sh
+++ util/git_hook_pre_commit.sh
@@ -1,4 +1,4 @@
-# chmod +x .git/hooks/pre-push
-# chmod +x util/git_hook_pre_push.sh
+# chmod +x .git/hooks/pre-commit
+# chmod +x util/git_hook_pre_commit.sh
 cat VERSION | convert -background none -density 196 -fill black -pointsize 21 -resample 72 -unsharp 0x.5 text:- -trim +repage -bordercolor white -border 3 util/minetest_wadsprint_version.png
 git add util/minetest_wadsprint_version.png
