9eb39294ad4fb86db78e0d86973e8692b2c4df25
aa6 <justsunshineandbluesky@gmail.com>
2016-04-30 12:29:35 +0300

    Autoversioning

--- util/git_hook_pre_commit.sh
+++ util/git_hook_pre_commit.sh
@@ -1,4 +1,9 @@
 # chmod +x .git/hooks/pre-commit
 # chmod +x util/git_hook_pre_commit.sh
+source util/increment_version.sh
+VERSION=`cat VERSION`
+VERSION=`increment_version $VERSION`
+echo -n $VERSION > VERSION
 cat VERSION | convert -background none -density 196 -fill black -pointsize 21 -resample 72 -unsharp 0x.5 text:- -trim +repage -bordercolor white -border 3 util/minetest_wadsprint_version.png
+git add VERSION
 git add util/minetest_wadsprint_version.png
