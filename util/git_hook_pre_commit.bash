a2ad65ef1d325daf04d009725f341d25b4f15f62
aa6 <justsunshineandbluesky@gmail.com>
2016-07-22 20:21:23 +0300

    Fixing version incrementor broken by imagetragick fix.

--- util/git_hook_pre_commit.bash
+++ util/git_hook_pre_commit.bash
@@ -5,6 +5,6 @@ source util/increment_version.bash
 VERSION=`cat VERSION`
 VERSION=`increment_version $VERSION`
 echo -n $VERSION > VERSION
-cat VERSION | convert -background none -density 196 -fill black -pointsize 21 -resample 72 -unsharp 0x.5 text:- -trim +repage -bordercolor white -border 3 util/minetest_wadsprint_version.png
+convert -background none -density 196 -fill black -pointsize 21 -resample 72 -unsharp 0x.5 label:"$(cat VERSION)" -trim +repage -bordercolor white -border 3 util/minetest_wadsprint_version.png
 git add VERSION
 git add util/minetest_wadsprint_version.png
