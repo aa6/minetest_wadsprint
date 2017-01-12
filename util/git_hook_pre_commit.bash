48f196fc1cacb693f3c6744a5bdfbb7b21c9c267
aa6 <justsunshineandbluesky@gmail.com>
2017-01-12 18:42:57 +0300

    Version incrementor update

--- util/git_hook_pre_commit.bash
+++ util/git_hook_pre_commit.bash
@@ -1,10 +1,18 @@
 #!/bin/bash
-# chmod +x .git/hooks/pre-commit
-# chmod +x util/git_hook_pre_commit.bash
-source util/increment_version.bash
-VERSION=`cat VERSION`
-VERSION=`increment_version $VERSION`
-echo -n $VERSION > VERSION
-convert -background none -density 196 -fill black -pointsize 21 -resample 72 -unsharp 0x.5 label:"$(cat VERSION)" -trim +repage -bordercolor white -border 3 util/minetest_wadsprint_version.png
-git add VERSION
-git add util/minetest_wadsprint_version.png
+case $1 in
+  "install")
+    SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    echo -n "bash util/git_hook_pre_commit.bash execute" > $SCRIPT_DIRECTORY/../.git/hooks/pre-commit
+    chmod +x $SCRIPT_DIRECTORY/../.git/hooks/pre-commit
+    chmod +x $SCRIPT_DIRECTORY/git_hook_pre_commit.bash
+    ;;
+  "execute")
+    source util/increment_version.bash
+    VERSION=`cat VERSION`
+    VERSION=`increment_version $VERSION`
+    echo -n $VERSION > VERSION
+    convert -background none -density 196 -fill black -pointsize 21 -resample 72 -unsharp 0x.5 label:"$(cat VERSION)" -trim +repage -bordercolor white -border 3 util/minetest_wadsprint_version.png
+    git add VERSION
+    git add util/minetest_wadsprint_version.png
+    ;;
+esac
