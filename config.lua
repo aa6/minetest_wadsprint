09a29f87ef841fd9c63ebce9285a836b78fa99de
aa6 <justsunshineandbluesky@gmail.com>
2017-08-10 14:59:42 +0300

    Allow to jump over two blocks with shift by default because it's fun.

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ minetest_wadsprint.PLAYERS_STATS_FILE_LIMIT_RECORDS                      = 1000
 minetest_wadsprint.PLAYER_STATS_UPDATE_PERIOD_SECONDS                    = 1
 minetest_wadsprint.PLAYER_CONTROLS_CHECK_PERIOD_SECONDS                  = 0.1
 minetest_wadsprint.SPRINT_SPEED_MODIFIER_COEFFICIENT                     = 3.8
-minetest_wadsprint.SPRINT_JUMP_HEIGHT_MODIFIER_COEFFICIENT               = 1.1
+minetest_wadsprint.SPRINT_JUMP_HEIGHT_MODIFIER_COEFFICIENT               = 1.2
 minetest_wadsprint.SPRINT_STAMINA_DECREASE_PER_UPDATE_PERIOD_COEFFICIENT = 0.0005 -- 0.1 means 10% decrease
 minetest_wadsprint.SPRINT_STAMINA_INCREASE_PER_UPDATE_PERIOD_COEFFICIENT = 0.0001 -- 0.1 means 10% increase
 -- Config values for `hudbars` mod (totally optional, applied only if installed).
